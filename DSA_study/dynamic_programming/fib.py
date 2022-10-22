7ff014ff8b3ac25520392e36b91b37ee806347a1
Garret G <g.grant92@gmail.com>
2022-10-22 16:17:02 -0700

    memoization fib complete

---
+++ DSA_study/dynamic_programming/fib.py
@@ -0,0 +1,11 @@
+def fib(n, memo = {}):
+    """Takes in an integer and returns the corresponding fib sequence"""
+    if n in memo: return memo[n]
+    if n <= 2: return 1
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
+
+print(fib(6))
+print(fib(7))
+print(fib(8))
+print(fib(1000))
